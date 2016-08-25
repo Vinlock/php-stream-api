2e39b57ca64948a9be2729fef8a7db4dfe703ec5
Vinlock <dak@pvpallday.com>
2016-08-25 08:22:19 -0700

    Code Adjustments

--- src/StreamDriver.php
+++ src/StreamDriver.php
@@ -76,21 +76,4 @@ abstract class StreamDriver {
         self::$limit = $limit;
     }
 
-    /**
-     * Get the JSON data with CURL
-     *
-     * @param $url
-     * @return mixed
-     */
-    // final private static function curl_get_contents($url) {
-    //     $ch = curl_init($url);
-    //     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    //     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    //     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-    //     curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-    //     $data = curl_exec($ch);
-    //     curl_close($ch);
-    //     return $data;
-    // }
-
 }
